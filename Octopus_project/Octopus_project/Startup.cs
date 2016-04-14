f9f1e148e51a028fe1dd77f2b0729b15818718d4
Sergey <sergey.ior97@gmail.com>
2016-04-14 09:34:29 +0300

    initial commit (add empty project MVC 5, Identity 2, Entity 6)

---
+++ Octopus_project/Octopus_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Octopus_project.Startup))]
+namespace Octopus_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
